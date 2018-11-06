6c5d366c3c0e36a3beb4d1c97d616fffe178179e
Izz Noland <izz@linux.com>
2018-11-06 08:02:12 -0600

    added to default

--- cookbooks/daxko_asset_mgmt/recipes/default.rb
+++ cookbooks/daxko_asset_mgmt/recipes/default.rb
@@ -1,5 +1,27 @@
-#
-# Cookbook:: daxko_asset_mgmt
-# Recipe:: default
-#
-# Copyright:: 2018, The Authors, All Rights Reserved.
+package 'httpd'
+
+service 'httpd' do
+  action [:enable, :start]
+end
+
+template '/var/www/html/index.php' do
+  source 'index.php.erb'
+end
+
+template '/var/www/html/search.php' do
+  source 'search.php.erb'
+end
+
+template '/var/www/html/add.php' do
+  source 'add.php.erb'
+end
+
+package 'wget'
+
+#yum_repository 'mysql-community' do
+#  baseurl 'https://dev.mysql.com/get/mysql80-community-release-el7-1.noarch.rpm'
+#end
+
+execute 'mysql_repo' do
+  command '/usr/bin/yum install https://dev.mysql.com/get/mysql80-community-release-el7-1.noarch.rpm'
+end
